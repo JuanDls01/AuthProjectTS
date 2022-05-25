3d9710fe195d89b5601f40a6e43cb291838e7264
unknown <juanignaciodelossantos01@gmail.com>
2022-05-25 11:47:51 -0300

    Add failed validateToken function

--- api/src/utils/jwt.utils.ts
+++ api/src/utils/jwt.utils.ts
@@ -1,5 +1,6 @@
 // This function generate a JSON Web Token:
-import { sign, SignOptions } from 'jsonwebtoken';
+import { verify } from 'crypto';
+import { sign, SignOptions, VerifyOptions } from 'jsonwebtoken';
 import config from '../../lib/config';
 
 export const generateAccessToken = (user:object): string => {
@@ -11,4 +12,26 @@ export const generateAccessToken = (user:object): string => {
     return accessToken
 };
 
-// Checks if JWT token is valid:
+// interface TokenPayload {
+//     exp: number;
+//     axxessTypes: string[];
+//     name: string;
+//     userId:number;
+// }
+
+// // Checks if JWT token is valid:
+// export function validateToken(token: string):
+// Promise<TokenPayload> {
+//     const publicKey: string = config.publicKey;
+
+//     const verifyOptions: VerifyOptions = {
+//         algorithms: ['RS256'],
+//     }
+
+//     return new Promise((resolve, reject) => {
+//         verify(token, publicKey, verifyOptions, (error, decoded: TokenPayload) => {
+//             if (error) return reject(error);
+//             resolve(decoded);
+//         })
+//     })
+// }
