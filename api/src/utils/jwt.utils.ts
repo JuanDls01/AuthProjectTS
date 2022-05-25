b075906baec33c54cbb890bf3082db61077d2293
unknown <juanignaciodelossantos01@gmail.com>
2022-05-25 11:47:26 -0300

    Add failed validateToken function

--- api/src/utils/jwt.utils.ts
+++ api/src/utils/jwt.utils.ts
@@ -2,10 +2,13 @@
 import { sign, SignOptions } from 'jsonwebtoken';
 import config from '../../lib/config';
 
-export const generateAccessToken = (user:object) => {
-    // For create the access token we need to pass to sign a payload (it's the user information), and the private key
+export const generateAccessToken = (user:object): string => {
+    // This function create the access token, we have to pass a payload (it's the user information),
+    // and the private key to the sign function. 
     const secret:string = config.privateKey;
     const accessToken: string = sign(user, secret);
     console.log(accessToken);
     return accessToken
-}
+};
+
+// Checks if JWT token is valid:
